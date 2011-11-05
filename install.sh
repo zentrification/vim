e13eace786ca7b9f85ff9b5ad9dae6efb8f5b1b0
Chris Dahms <zentrification@gmail.com>
2011-11-04 20:45:36 -0400

    made bash < <curl(url) style installer

--- install.sh
+++
@@ -1,6 +0,0 @@
-tar cfz ~/.vimbackup.tgz ~/.vim ~/.vimrc
-rm -rf ~/.vim ~/.vimrc
-git clone git://github.com/zentrification/vim.git /tmp/zentrification-vim
-mv /tmp/zentrification-vim ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
-cd ~/.vim && git submodule init && git submodule update
